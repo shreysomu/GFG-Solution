06deb1896874d1cd71e4c226f90c6b9866d5618d
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-07-31 08:12:00 +0530

    Create Elements in the Range.cpp

---
+++ Elements in the Range.cpp
@@ -0,0 +1,22 @@
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+		// Your code goes here
+		set<int>S;
+		for(int i=0;i<n;i++)
+		 S.insert(arr[i]);
+		 for(int i=A;i<=B;i++)
+		 
+		 {
+		     if(S.find(i)==S.end())
+		     return false;
+		 }
+	}
+
+
+		 
+
+};
