96a916436bc90f820de3ff4dc8071a7e2916b5f5
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-09-03 10:15:03 +0530

    Create Start Coding.cpp

---
+++ Start Coding.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+void printHello();
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to print "Hello World"
+void printHello(){
+    
+    // Your code goes here
+    cout<<"Hello World";
+    
+}
