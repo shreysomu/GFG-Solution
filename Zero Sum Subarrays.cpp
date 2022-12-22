03f2ca3dc72be01004427e01bfc35708f29d57a3
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-12-22 10:43:14 +0530

    Create Zero Sum Subarrays.cpp

---
+++ Zero Sum Subarrays.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to count subarrays with sum equal to 0.
+    long long int findSubarray(vector<long long int> &arr, int n ) {
+        //code here
+        unordered_map<long,long>mp;
+        long long Count=0,Sum=0;
+        for(long long i=0;i<n;i++)
+        {
+            mp[Sum]++;
+            Sum+=arr[i];
+            Count+=mp[Sum];
+        }
+        return Count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n; //input size of array
+       
+        vector<long long int> arr(n,0);
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i]; //input array elements
+        Solution ob;
+        cout << ob.findSubarray(arr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
